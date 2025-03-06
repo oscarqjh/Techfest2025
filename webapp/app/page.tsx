22228cd25f48f83e1cd699c6c1cf7ff12c9a2d50
oscarqjh <oscar.jh9@gmail.com>
2025-03-06 21:31:25 +0800

    base layout done

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,8 +1,50 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+import { Input } from "@/components/ui/input";
+import { ShimmerInput } from "@/components/magicui/shimmer-input";
+import { HeroInput } from "@/components/ui/hero-input";
+import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
+
+const placeholders = [
+  "Enter your URL here",
+  "https://www.straitstimes.com/",
+  "https://www.channelnewsasia.com/",
+  "https://www.todayonline.com/",
+];
 
 export default function Home() {
+  const [url, setUrl] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const [result, setResult] = React.useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(url);
+    try {
+      const response = await fetch("/api/ask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setResult(data.answer);
+      } else {
+        setError(data.error);
+      }
+    } catch (error) {
+      setError("An error occurred, please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       {/* <nav className="my-16 animate-fade-in">
@@ -24,46 +66,42 @@ export default function Home() {
         quantity={100}
       />
       <div className="flex items-center justify-center">
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
           <div className="flex items-center justify-center mr-10">
             <span>Are</span>
           </div>
         </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
           <div className="flex items-center justify-center mr-10">
             <span>U</span>
           </div>
         </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
           <div className="flex items-center justify-center">
             <span>R</span>
           </div>
         </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
           <div className="flex items-center justify-center">
             <span>ea</span>
           </div>
         </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
           <div className="flex items-center justify-center">
-            <span>L</span>
+            <span>L?</span>
           </div>
         </h1>
       </div>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link>{" "}
-          to solve API authentication and authorization for developers.
-        </h2>
+      <div className="w-[40%] my-16 text-center animate-fade-in">
+        <div className="h-[7rem] flex flex-col justify-center  items-center px-4">
+          <PlaceholdersAndVanishInput
+            placeholders={placeholders}
+            onChange={(e) => setUrl(e.target.value)}
+            onSubmit={handleSubmit}
+          />
+        </div>
       </div>
     </div>
   );
