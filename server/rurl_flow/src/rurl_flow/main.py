38bbe24c89bbcf4f263997c16f5ba7cbd1cb3222
oscarqjh <oscar.jh9@gmail.com>
2025-03-08 19:40:57 +0800

    Refactor RURLFlow to support asynchronous web parsing and analysis, enhancing URL handling and performance

--- server/rurl_flow/src/rurl_flow/main.py
+++ server/rurl_flow/src/rurl_flow/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from random import randint
 import sys
+import time
+import asyncio
 
 from pydantic import BaseModel
 
@@ -31,38 +33,50 @@ class RURLState(BaseModel):
 
 class RURLFlow(Flow[RURLState]):
 
+    def __init__(self, url, **kwargs):
+      super().__init__(**kwargs)
+      self.url = url
+
     @start()
-    def parse_web(self):
-        print("Parsing contents of the web article: ", self.state.weblink)
+    async def parse_web(self):
+        print("Parsing contents of the web article: ", self.url)
 
         web_parser = WebParsingTool()
-        result = web_parser._run(self.state.weblink)
+        result = web_parser._run(self.url)
         print("Result = ",  result)
         self.state.title = result.get("title","")
         self.state.content = result.get("content","")
         self.state.image_urls = result.get("image_links",[])
         self.state.date = result.get("date","")
 
+        return result
+
     @listen(parse_web)
-    async def web_analyse(self):
+    async def web_analyse(self, data: dict):
         print("Analysing the web article")
         web_analyser = WebAnalyserTool()
-        analyser_input = {
-            "data": {
-                "title": self.state.title,
-                "weblink": self.state.weblink,
-                "image_urls": self.state.image_urls,
-                "content": self.state.content,
-                "date": self.state.date
-            }
-        }
+        # analyser_input = {
+        #     "data": {
+        #         "title": self.state.title,
+        #         "weblink": self.state.weblink,
+        #         "image_urls": self.state.image_urls,
+        #         "content": self.state.content,
+        #         "date": self.state.date
+        #     }
+        # }
 
-        result = await web_analyser._run(analyser_input)
+        input_data = {}
+        input_data["data"] = data
+
+        result = web_analyser._run(input_data)
         self.state.web_analyse_results = result
+
+        return result
 
     # Analysts crew
     @listen(parse_web)
     def analyse_image_and_text(self):
+        return
         print("Analysing the image and text")
         result = (
             Analysts()
@@ -74,6 +88,7 @@ class RURLFlow(Flow[RURLState]):
     # Image Forensics crew
     @listen(parse_web)
     async def detect_forgery(self):
+        return
         print("Analysing image for forgery or deepfakes")
         result = await (
             ImageForensics()
@@ -85,6 +100,7 @@ class RURLFlow(Flow[RURLState]):
     # Validator crew
     @listen(parse_web)
     async def internal_validation(self):
+        return
         print("Validating source credibility")
         result = await (
             Validators()
@@ -95,14 +111,11 @@ class RURLFlow(Flow[RURLState]):
 
     # Researchers crew
     @listen(web_analyse)
-    async def web_research(self):
+    async def web_research(self, data: dict):
         print("Generating fact checks")
 
-        # get article body from data
-        article_body = self.state.web_analyse_results['article_body']
-
         # run web search
-        result = await web_search_node(article_body)
+        result = await web_search_node(data)
 
         # append result
         self.state.web_research_results = result
@@ -113,9 +126,11 @@ class RURLFlow(Flow[RURLState]):
         
 
 def kickoff():
-    rurl_flow = RURLFlow()
-    rurl_flow.kickoff(inputs={"weblink":"https://www.straitstimes.com/opinion/forum/forum-singapores-foreign-policy-based-on-realism"})
+    t0 = time.time()
+    rurl_flow = RURLFlow(url="https://www.straitstimes.com/opinion/forum/forum-singapores-foreign-policy-based-on-realism")
+    res = asyncio.run(rurl_flow.kickoff_async())
 
+    print("Time taken = ", time.time()-t0)
 
 def plot():
     rurl_flow = RURLFlow()
