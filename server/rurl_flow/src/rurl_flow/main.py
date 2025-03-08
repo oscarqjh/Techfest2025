bf19ce8c038f4a6eff26eb12b6660a532819f485
chengyaolee <chengyao.lee@gmail.com>
2025-03-08 15:01:38 +0800

    Rename crew files

--- server/rurl_flow/src/rurl_flow/main.py
+++ server/rurl_flow/src/rurl_flow/main.py
@@ -5,22 +5,62 @@ from pydantic import BaseModel
 
 from crewai.flow import Flow, listen, start
 
-from rurl_flow.crews.poem_crew.poem_crew import PoemCrew
+from .crews.poem_crew.poem_crew import PoemCrew
+
+from .crews.analysts.src.analysts.analysts import Analysts
+from .crews.image_forensics.src.image_forensics.image_forensics import ImageForensics
+from .crews.researchers.src.researchers.researchers import Researchers
+from .crews.validators.src.validators.validators import Validators
 
 
-class PoemState(BaseModel):
-    sentence_count: int = 1
-    poem: str = ""
+class RURLState(BaseModel):
+    weblink: str
+    title: str
+    content: str
+    image_links: list[str]
+    date: str
 
 
-class PoemFlow(Flow[PoemState]):
+class RURLFlow(Flow[RURLState]):
 
     @start()
-    def generate_sentence_count(self):
-        print("Generating sentence count")
-        self.state.sentence_count = randint(1, 5)
+    def parse_web_article(self, weblink: str):
+        print("Parsing contents of the web article: ", self.state.weblink)
+        result = (
+            Analysts()
+            .web_parsing_task()
+            .kickoff(inputs={"url": self.state.weblink})
+        )
+        self.title = result.title
+        self.content = result.content
+        self.image_links = result.image_links
+        self.date = result.date
 
-    @listen(generate_sentence_count)
+    @listen(parse_web_article)
+    def analyse_article(self):
+        print("Analysing the article")
+        result = (
+            Researchers()
+            .article_analysis_task()
+            .kickoff(inputs={"body": self.state.content})
+        )
+    
+    def analyse_image_and_text(self):
+        print("Analysing the image and text")
+        result = (
+            ImageForensics()
+            .image_text_analysis_task()
+            .kickoff(inputs={"url": self.state.w})
+        )
+
+    def detect_forgery(self):
+        print("Analysing image for forgery or deepfakes")
+        result = (
+            ImageForensics()
+            .crew()
+            .kickoff(inputs={"image_links": self.state.image_urls})
+        )
+
     def generate_poem(self):
         print("Generating poem")
         result = (
