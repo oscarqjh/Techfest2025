5161a5db5a4669f5f933ea8c686019d490ac0771
oscarqjh <oscar.jh9@gmail.com>
2025-03-09 18:04:35 +0800

    Add hello_world endpoint to FastAPI for greeting message

--- server/main.py
+++ server/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/api/v1/helloworld")
+def hello_world():
+    return {"message": "Hello, World!"}
