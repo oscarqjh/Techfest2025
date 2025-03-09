2675b63d38417cb17a4ebc2987c6ee818e2f2c1a
oscarqjh <oscar.jh9@gmail.com>
2025-03-10 00:31:31 +0800

    Refactor Home component to include scroll behavior on loading and update result handling

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Particles from "./components/particles";
 import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
 import ResultSkeleton from "./components/result-skeleton";
@@ -19,10 +19,12 @@ export default function Home() {
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState("");
   const [result, setResult] = React.useState("");
+  const resultSectionRef = useRef<HTMLDivElement | null>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
+
     try {
       const response = await fetch("/api/validate", {
         method: "GET",
@@ -34,7 +36,7 @@ export default function Home() {
       const data = await response.json();
       console.log(data);
       if (response.ok) {
-        setResult(data.answer);
+        setResult(data);
       } else {
         setError(data.error);
       }
@@ -48,56 +50,66 @@ export default function Home() {
     }
   };
 
+  useEffect(() => {
+    if (loading) {
+      window.scrollBy({ top: 700, behavior: "smooth" }); // Scroll down 500px
+    }
+  }, [loading]);
+
   return (
     <div className="flex flex-col items-center justify-center w-screen min-h-screen h-fit overflow-x-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <div className="overflow-x-hidden hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
-      />
-      <div className="flex items-center justify-center mt-[10%]">
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-          <div className="flex items-center justify-center mr-10">
-            <span>Are</span>
-          </div>
-        </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-          <div className="flex items-center justify-center mr-10">
-            <span>U</span>
-          </div>
-        </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-          <div className="flex items-center justify-center">
-            <span>R</span>
-          </div>
-        </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-          <div className="flex items-center justify-center">
-            <span>ea</span>
-          </div>
-        </h1>
-        <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-          <div className="flex items-center justify-center">
-            <span>L?</span>
-          </div>
-        </h1>
-      </div>
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
+        <div className="overflow-x-hidden hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <Particles
+          className="absolute inset-0 -z-10 animate-fade-in"
+          quantity={100}
+        />
+        <div className="flex items-center justify-center">
+          <h1 className=" px-0.5 z-10 text-4xl text-transparent duration-3000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+            <div className="flex items-center justify-center mr-10">
+              <span>Are</span>
+            </div>
+          </h1>
+          <h1 className=" px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+            <div className="flex items-center justify-center mr-10">
+              <span>U</span>
+            </div>
+          </h1>
+          <h1 className=" px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+            <div className="flex items-center justify-center">
+              <span>R</span>
+            </div>
+          </h1>
+          <h1 className=" px-0.5 z-10 text-4xl text-transparent duration-3000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+            <div className="flex items-center justify-center">
+              <span>ea</span>
+            </div>
+          </h1>
+          <h1 className=" px-0.5 z-10 text-4xl text-transparent duration-3000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+            <div className="flex items-center justify-center">
+              <span>L?</span>
+            </div>
+          </h1>
+        </div>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="w-[40%] my-16 text-center animate-fade-in">
-        <div className="h-[7rem] flex flex-col justify-center  items-center px-4">
-          <PlaceholdersAndVanishInput
-            placeholders={placeholders}
-            onChange={(e) => setUrl(e.target.value)}
-            onSubmit={handleSubmit}
-            isLoading={loading}
-          />
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <div className="w-[40%] my-16 text-center animate-fade-in">
+          <div className="h-[7rem] flex flex-col justify-center  items-center px-4">
+            <PlaceholdersAndVanishInput
+              placeholders={placeholders}
+              onChange={(e) => setUrl(e.target.value)}
+              onSubmit={handleSubmit}
+              isLoading={loading}
+            />
+          </div>
         </div>
       </div>
 
       {/** Result skeleton*/}
       {loading && <ResultSkeleton />}
-      {result !== "" && !loading && <ResultSection data={result} />}
+      {result !== "" && !loading && (
+        <ResultSection ref={resultSectionRef} data={result} />
+      )}
     </div>
   );
 }
