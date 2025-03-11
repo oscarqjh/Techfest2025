4050df909f41cf60e0f8e368db0e84ef509ca380
oscarqjh <oscar.jh9@gmail.com>
2025-03-11 22:05:04 +0800

    ai card

---
+++ webapp/app/components/ai-card.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { animate, motion } from "motion/react";
+import React, { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+
+export function CardDemo() {
+  return (
+    <Card>
+      <CardTitle>Damn good card</CardTitle>
+      <CardDescription>
+        A card that showcases a set of tools that you use to create your
+        product.
+      </CardDescription>
+    </Card>
+  );
+}
+
+export const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "w-full mx-auto h-fit p-8 rounded-xl border border-[rgba(255,255,255,0.10)] bg-[rgba(40,40,40,0.70)]  shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset] group",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const CardTitle = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h3 className={cn("text-xl font-bold text-zinc-200 py-2", className)}>
+      {children}
+    </h3>
+  );
+};
+
+export const CardDescription = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <p className={cn("text-sm font-normal text-neutral-300", className)}>
+      {children}
+    </p>
+  );
+};
---
+++ webapp/app/components/ai-insights.tsx
@@ -0,0 +1,37 @@
+import { AnimatedCircularProgressBar } from "@/components/magicui/animated-circular-progress-bar";
+import { Card, CardDescription, CardTitle } from "./ai-card";
+import { BackgroundLines } from "@/components/ui/background-lines";
+
+interface AiInsightsProps {
+  data: any;
+}
+
+export default function AiInsights({ data }: AiInsightsProps) {
+  return (
+    <div className="text-zinc-300 text-justify">
+      <Card className="relative w-[88%]">
+        <BackgroundLines className="absolute inset-0 bg-transparent">
+          <div></div>
+        </BackgroundLines>
+        <div className="flex">
+          <div className="flex flex-col">
+            <CardTitle className="cursor-default">AI Insights</CardTitle>
+            <CardDescription className="mt-4">
+              {data.insights.raw}
+            </CardDescription>
+          </div>
+          <div className="flex flex-col items-center justify-center w-[20%] ml-14 cursor-default">
+            <p className="mb-4 font-bold cursor-default">Article Score</p>
+            <AnimatedCircularProgressBar
+              max={100}
+              min={0}
+              value={80}
+              gaugePrimaryColor="rgb(79 70 229)"
+              gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
+            />
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
